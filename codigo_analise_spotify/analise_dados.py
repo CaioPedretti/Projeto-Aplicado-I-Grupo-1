f36a74e68d339308d2bbcf360e539c54a04e2973
CaioPedretti <caiobalista@gmail.com>
2026-03-20 13:48:08 -0300

    Update analise_dados.py

--- codigo_analise_spotify/analise_dados.py
+++ codigo_analise_spotify/analise_dados.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+# Carregar dataset
+df = pd.read_csv('dados_spotify/dataset_spotify.csv')
+
+# Informações gerais
+print("Informações do dataset:")
+print(df.info())
+
+# Primeiras linhas
+print("\nPrimeiras linhas:")
+print(df.head())
